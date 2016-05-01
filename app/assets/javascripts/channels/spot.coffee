911d8e085a633b1682767704552e5b2a4e8c8921
Yaro <osyaroslav@gmail.com>
2016-05-01 19:33:20 +0300

    Change layout

--- app/assets/javascripts/channels/spot.coffee
+++ app/assets/javascripts/channels/spot.coffee
@@ -1,3 +1,4 @@
+return null;
 App.spot = App.cable.subscriptions.create { channel: "SpotChannel", spot_id: location.pathname.replace('/spots/', '') },
   connected: (data) ->
     # Called when the subscription is ready for use on the server
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,9 +11,6 @@ class HomeController < ApplicationController
       marker.lat spot.latitude
       marker.lng spot.longitude
       marker.infowindow spot.description
-      marker.picture({ url: 'http://people.mozilla.com/~faaborg/files/shiretoko/firefoxIcon/firefox-32.png',
-        width: 32, height: 32
-      })
       marker.json({
         id: spot.id,
         title: spot.title
